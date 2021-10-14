59c4832bf37017e20fc86bb456d2252c69f92aa2
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-10-14 10:58:10 +0200

    sd

--- controllers/c4job/c4job.go
+++ controllers/c4job/c4job.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	//_ "github.com/johannes-kuhfuss/services_utils/rest_errors"
+	_ "github.com/johannes-kuhfuss/services_utils"
 )
 
 func CreateC4Job(c *gin.Context) {
