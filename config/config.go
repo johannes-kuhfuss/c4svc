b95482584bbd792ffb66b496575c357832a45a28
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-10-28 16:00:07 +0200

    Config entries

--- config/config.go
+++ config/config.go
@@ -9,13 +9,15 @@ import (
 )
 
 var (
-	ginMode           = "debug" // release, debug, test
-	ShutDown          = false
-	NoJobWaitTime     = (time.Second * 10)
-	DeleteFinishedAge = (time.Hour * 1)
-	DeleteFailedAge   = (time.Hour * 2)
-	WarnStatusAge     = (time.Hour * 5)
-	CleanupWaitTime   = (time.Hour * 1)
+	ginMode            = "debug" // release, debug, test
+	ShutDown           = false
+	NoJobWaitTime      = (time.Second * 10)
+	DeleteFinishedAge  = (time.Hour * 1)
+	DeleteFailedAge    = (time.Hour * 2)
+	WarnStatusAge      = (time.Hour * 5)
+	CleanupWaitTime    = (time.Hour * 1)
+	StorageAccountName = ""
+	StorageAccountKey  = ""
 )
 
 func init() {
@@ -24,6 +26,8 @@ func init() {
 	if osGinMode == gin.ReleaseMode || osGinMode == gin.DebugMode || osGinMode == gin.TestMode {
 		ginMode = osGinMode
 	}
+	StorageAccountName = os.Getenv("STORAGE_ACCOUNT_NAME")
+	StorageAccountKey = os.Getenv("STORAGE_ACCOUNT_KEY")
 	logger.Info("Done initalizing configuration")
 }
 
